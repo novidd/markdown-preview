7b04ed08650cf31f01f324d4e1e04a495beae443
Lucas Pettersson <clm.pettersson@gmail.com>
2020-07-05 15:45:17 +0200

    Markdown previewer finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import marked from "marked";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const placeholder = 
+`# This is a bold header
+## This a subheader
+
+**Bold text**
+
+[This is a link](https://www.google.com "Google's homepage")
+
+\`const succes = "I am inline code";\`
+
+\`\`\`
+const succesBlock = () => {
+  console.log("I am block code"); 
+}
+succesBlock();
+\`\`\`
+
+1. I am a ordered list
+1. I am a ordered list
+
+> I am a block quote
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Image")
+`;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: placeholder
+    }
+  this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value
+    });
+  }
+  render() {
+    document.getElementById('preview').innerHTML = marked(this.state.markdown);
+    marked.setOptions({
+      "breaks": true
+    });
+    return (
+      <div className="App">
+        <h1>Editor</h1>
+        <textarea onChange={this.handleChange} id="editor">{this.state.markdown}</textarea>
+      </div>
+    );
+  }
 }
 
 export default App;
